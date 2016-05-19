e048b634c2305bc5d8dd7376fc1e91ed8f9caaad
Michael Henry <michaelhenry91@gmail.com>
2016-05-18 20:28:55 -0400

    Added files

---
+++ app.rb
@@ -0,0 +1,11 @@
+require 'sinatra'
+
+class MyWebApp < Sinatra::Base
+  get '/' do
+    "This is my app! Just in case you arrived here from the past via flux capacitor technology, the current year is { Time.now.year }"
+  end
+
+  get '/about' do
+    "Want to learn about me?  Give me a call! 555.555.5555"
+  end
+end
